d0cacd86078d1dc715d8eae79e973e73012e2de8
bimaryan <bimaryan046@gmail.com>
2023-12-28 19:48:10 +0700

    fix NotFound

--- src/App.js
+++ src/App.js
@@ -1,27 +1,25 @@
 // src/App.js
 import React from "react";
-import { SpeedInsights } from "@vercel/speed-insights/react"
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import Navbar from "./components/Navbar";
 import RegistrationForm from "./components/RegistrationForm";
 import Home from "./components/Home";
 import Footer from "./components/Footer";
 import NotFound from './components/NotFound';
-import { Switch, Route, Routes, BrowserRouter as Router } from 'react-router-dom';
 
 const App = () => {
   return (
     <div>
       <Router>
         <Navbar />
-        <Switch>
-          <Routes>
-            <Route path="/" element={<Home />} />
-            {/* <Route path="/it-comp-2024" element={<ITComp2024 />} /> */}
-            {/* <Route path="/it-comp-internal-2024" element={<ITCompInternal2024 />} /> */}
-            <Route path="/regist" element={<RegistrationForm />} />
-            <Route element={NotFound} />
-          </Routes>
-        </Switch>
+        <Routes>
+          <Route path="/" element={<Home />} />
+          {/* Uncomment and replace with your components */}
+          {/* <Route path="/it-comp-2024" element={<ITComp2024 />} /> */}
+          {/* <Route path="/it-comp-internal-2024" element={<ITCompInternal2024 />} /> */}
+          <Route path="/regist" element={<RegistrationForm />} />
+          <Route path="*" element={<NotFound />} />
+        </Routes>
         <Footer />
       </Router>
     </div>
