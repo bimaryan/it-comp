27eca283c59facfafe5add1d635fa65a5c26f324
bimaryan <bimaryan046@gmail.com>
2023-12-28 19:46:18 +0700

    NoutFound

--- src/App.js
+++ src/App.js
@@ -5,20 +5,24 @@ import Navbar from "./components/Navbar";
 import RegistrationForm from "./components/RegistrationForm";
 import Home from "./components/Home";
 import Footer from "./components/Footer";
-import { Route, Routes, BrowserRouter as Router } from 'react-router-dom';
+import NotFound from './components/NotFound';
+import { Switch, Route, Routes, BrowserRouter as Router } from 'react-router-dom';
 
 const App = () => {
   return (
     <div>
       <Router>
         <Navbar />
-        <Routes>
-          <Route path="/" element={<Home />} />
-          {/* <Route path="/it-comp-2024" element={<ITComp2024 />} /> */}
-          {/* <Route path="/it-comp-internal-2024" element={<ITCompInternal2024 />} /> */}
-          <Route path="/regist" element={<RegistrationForm />} />
-        </Routes>
-        <Footer/>
+        <Switch>
+          <Routes>
+            <Route path="/" element={<Home />} />
+            {/* <Route path="/it-comp-2024" element={<ITComp2024 />} /> */}
+            {/* <Route path="/it-comp-internal-2024" element={<ITCompInternal2024 />} /> */}
+            <Route path="/regist" element={<RegistrationForm />} />
+            <Route element={NotFound} />
+          </Routes>
+        </Switch>
+        <Footer />
       </Router>
     </div>
   );
---
+++ src/components/NotFound.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+const NotFound = () => {
+  return (
+    <div className="container text-center my-5">
+      <h1 className="display-1">404</h1>
+      <p className="lead">Page not found</p>
+      <p>Sorry, the page you are looking for might be in another castle.</p>
+    </div>
+  );
+};
+
+export default NotFound;
